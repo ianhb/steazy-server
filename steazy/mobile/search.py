05c68cc6fe27a78f923c3e839cd88d376c2fa0b7
Ian Blakley <ian@810labs.me>
2015-07-11 12:35:02 -0700

    stzsvr-5 Authenticate Spotify Requests Added client credentials to Spotify search requests

--- steazy/mobile/search.py
+++ steazy/mobile/search.py
@@ -1,39 +1,65 @@
+import calendar
 import json
 import urllib2
+import time
 
 from django.db.models import Q
+import requests
 import soundcloud as soundcloud
 
 from models import Song
+import spotifyviews
 
 __author__ = 'Ian'
 
 
+class Spotify:
+    def __init__(self):
+        self.TOKEN_EXPIRES = calendar.timegm(time.gmtime())
+        self.TOKEN = None
+
+    def spotify_auth(self):
+        header = spotifyviews.HEADERS
+        body = {'grant_type': 'client_credentials'}
+
+        client_request = requests.post(spotifyviews.SPOTIFYURL, data=body, headers=header)
+
+        response = client_request.json()
+
+        self.TOKEN_EXPIRES = self.TOKEN_EXPIRES + response['expires_in']
+
+        return response['access_token']
+
+    def search_spotify(self, argument):
+        if calendar.timegm(time.gmtime()) > self.TOKEN_EXPIRES:
+            self.TOKEN = self.spotify_auth()
+        header = {'Authorization': 'Bearer ' + self.TOKEN}
+        argument = argument.replace(' ', '+')
+        data = requests.get('https://api.spotify.com/v1/search?q=' + argument + '%20&type=track', headers=header)
+        return data.json()
+
+    @staticmethod
+    def parse_spotify(data):
+        songs = []
+        for track in data['tracks']['items']:
+            if check_if_exists({'source': 'Spotify', 'tag': track['id']}):
+                songs.append(Song.objects.get(source='Spotify', tag=track['id']))
+            else:
+                artists = ''
+                for art in track['artists']:
+                    artists += unicode(art['name']) + ', '
+                artists = artists[:len(artists) - 2]
+                song = Song(name=unicode(track['name']), artist=artists, album=unicode(track['album']['name']),
+                            source='Spotify', tag=track['id'], inherited_popularity=track['popularity'])
+                song.save()
+                songs.append(song)
+        return songs
+
+
+SPOTIFY = Spotify()
+
 def check_if_exists(song_data):
     return Song.objects.filter(source=song_data['source'], tag=song_data['tag']).exists()
-
-
-def search_spotify(arguement):
-    arguement = arguement.replace(' ', '+')
-    data = json.load(urllib2.urlopen('https://api.spotify.com/v1/search?q=' + arguement + '%20&type=track'))
-    return data
-
-
-def parse_spotify(data):
-    songs = []
-    for track in data['tracks']['items']:
-        if check_if_exists({'source': 'Spotify', 'tag': track['id']}):
-            songs.append(Song.objects.get(source='Spotify', tag=track['id']))
-        else:
-            artists = ''
-            for art in track['artists']:
-                artists += unicode(art['name']) + ', '
-            artists = artists[:len(artists) - 2]
-            song = Song(name=unicode(track['name']), artist=artists, album=unicode(track['album']['name']),
-                        source='Spotify', tag=track['id'], inherited_popularity=track['popularity'])
-            song.save()
-            songs.append(song)
-    return songs
 
 
 def sort_songs(songs):
@@ -41,7 +67,7 @@ def sort_songs(songs):
 
 
 def search_songs(query):
-    return sort_songs(parse_spotify(search_spotify(query)) + parse_soundcloud(search_soundcloud(query)))
+    return sort_songs(Spotify.parse_spotify(SPOTIFY.search_spotify(query)) + parse_soundcloud(search_soundcloud(query)))
 
 
 def search_database(query):
